1daf58e05f6870719abe30525b254dd8e548e67e
dcwilbourne <dcwilbourne@gmail.com>
2021-02-27 01:40:31 -0500

    tweaked documentation

--- R/rcrsv_plot.R
+++ R/rcrsv_plot.R
@@ -1,12 +1,16 @@
-#' Plots Output Values for Recursive Sequence
+#' Plots Output for a (Specific) Recursive Sequence
 #'
-#' Plots the output values of a specific recursive sequence over
-#' time given a tibble with information on the sequence's values as input.
+#' For the recursive sequence xn = xn-1 + ((xn-3 - xn-2)/n), plots the output
+#' of the sequence given as input a tibble with information on the sequence's
+#' beginning values and the specific numbers in the sequence to plot.
 #'
 #' @param rcrsv_df
-#' a tibble first three digits in the sequence and the nth value to calculate
+#' A tibble with four columns: the first three digits in the sequence, and the
+#' nth value to calculate. The first three columns typically remain the same,
+#' while the last column increases.
 #'
-#' @return a plot of the output values for different values of n
+#' @return Returns a plot of the output.
+#'
 #' @export rcrsv_plot
 #'
 #' @examples
@@ -22,16 +26,36 @@
 #' 2,4,3,10,
 #' 2,4,3,12)
 #'
-#' rcrsv_plot(my_data) #plots the numbers in the sequence from column 4
+#' rcrsv_plot(my_data) # plots the nth outcomes of the sequence from the
+#' fourth column
+#'
+#' my_wrong_data <- tibble::tribble(
+#' ~x, ~y, ~z, ~n, ~p,
+#' 2,4,3,3,5,
+#' 2,4,3,4,5,
+#' 2,4,3,5,5,
+#' 2,4,3,6,5,
+#' 2,4,3,7,5,
+#' 2,4,3,8,5,
+#' 2,4,3,9,5,
+#' 2,4,3,10,5,
+#' 2,4,3,12,5)
+#'
+#' \dontrun{rcrsv_plot(my_wrong_data) # returns an error}
 #'
 
 rcrsv_plot <- function(rcrsv_df) {
+  # test inputs
   rcrsv_plot_checkinputs(rcrsv_df)
+
+  # write function
   rcrsv_df$output <- 0
+  # generate output column
   for (i in seq(nrow(rcrsv_df))) {
     rcrsv_df$output[i] <- rcrsv_solve(c(rcrsv_df[[i,1]], rcrsv_df[[i,2]], rcrsv_df[[i,3]]),
                                n = rcrsv_df[[i,4]])
   }
+  # plot output
   output_plot <- ggplot2::ggplot(rcrsv_df, ggplot2::aes(n, output)) +
     ggplot2::geom_line() +
     ggplot2::labs(title = "Recursive Output", x = "n", y = "Output")
@@ -39,24 +63,27 @@ rcrsv_plot <- function(rcrsv_df) {
 }
 
 rcrsv_plot_checkinputs <- function(rcrsv_df) {
+  # test that rcrsv_df is a dataframe
   if(!is.list(rcrsv_df)) {
     stop("input is not a list/dataframe")
   }
-  # test that x and n are of correct length
+  # test that rcrsv_df has four columns
   if(ncol(rcrsv_df) != 4) {
     stop("input does not have four columns")
   }
+  # test that each column is numeric
   for (i in seq(length(rcrsv_df))) {
     if(!is.numeric(rcrsv_df[,i] %>% unlist())) {
       stop("column " + i + " is not numeric")
     }
   }
-  # test that n is a positive integer
+  # test that column four contains only positive values
   for (i in seq(length(rcrsv_df[,4]))) {
     if(rcrsv_df[i,4] %>% unlist() <= 0) {
       stop("column 4 must be greater than zero")
     }
   }
+  # test that column four contains only integers
   for (i in seq(length(rcrsv_df[,4]))) {
     if(rcrsv_df[i,4] %>% unlist() %% 1 != 0) {
       stop("column 4 must be an integer")
--- R/rcrsv_solve.R
+++ R/rcrsv_solve.R
@@ -1,22 +1,22 @@
 #' Calculate New Value of (Specific) Recursive Sequence
 #'
-#' For the recursive sequence xn = xn−1 + ((xn−3 − xn−2)/n), returns the
+#' For the recursive sequence xn = xn-1 + ((xn-3 - xn-2)/n), returns the
 #' nth value in the sequence given the first three values.
 #'
-#' @param x a vector of the first three numbers in the sequence
-#' @param n the integer value in the sequence to return
+#' @param x A vector of the first three numbers in the sequence
+#' @param n The integer value in the sequence to return
 #'
-#' @return the value of the nth element in the sequence
+#' @return Returns the value of the nth element in the sequence
 #' @export rcrsv_solve
 #'
 #' @examples
 #' rcrsv_solve(c(2, 4, 3), 4) # returns 2.5
-#' \dontrun{rcrsv_solve(c(2, 4), 4) #returns an error}
+#' \dontrun{rcrsv_solve(c(2, 4), 4) # returns an error}
 rcrsv_solve <- function(x, n) {
-  #test inputs
+  # test inputs
   rcrsv_solve_checkinputs(x=x,n=n)
 
-  #write function
+  # write function
   results <-  vector(mode = "double")
   results[1:3] <-  x[1:3]
   for (i in seq(n)) {
